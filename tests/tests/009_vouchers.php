54a46730c3234e4f46597116823985b3c6caf5dd
Sebastian Lutz <slu@baebeca.de>
2021-02-28 12:42:36 +0100

    0064717: add mixed tax voucher test

--- tests/tests/009_vouchers.php
+++ tests/tests/009_vouchers.php
@@ -31,3 +31,43 @@ try {
 	test(print_r($e->get_error(), true));
 	test_finished(false);
 }
+
+test_start('create mixed tax voucher 16/19');
+try {
+    $request = $lexoffice->create_voucher([
+        'version' => 0,
+        'voucherDate' => '2021-02-27',
+        'dueDate' => '2021-03-06',
+        'type' => 'salesinvoice',
+        'voucherNumber' => "21-001003RP",
+        'useCollectiveContact' => true,
+        'totalGrossAmount' => 9974.69,
+        'voucherItems' => [
+            [
+                'amount' => "1748.81",
+                'taxAmount' => "279.81",
+                'taxRatePercent' => 16,
+                'categoryId' => '8f8664a1-fd86-11e1-a21f-0800200c9a66',
+            ],
+            [
+                'amount' => "6677.37",
+                'taxAmount' => "1268.70",
+                'taxRatePercent' => 19,
+                'categoryId' => '8f8664a1-fd86-11e1-a21f-0800200c9a66',
+            ],
+        ],
+        'taxType' => "net",
+        'totalTaxAmount' => '1548.51',
+    ]);
+
+    if ($request->id) {
+        test('voucher created - id: '.$request->id);
+        test_finished(true);
+    } else {
+        test_finished(false);
+    }
+} catch(lexoffice_exception $e) {
+    test($e->getMessage());
+    test(print_r($e->get_error(), true));
+    test_finished(false);
+}
