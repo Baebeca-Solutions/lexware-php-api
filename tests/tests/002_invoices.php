125c71ffdcac212d5f21a043b9700354da15991b
Sebastian Hayer-Lutz <slu@baebeca.de>
2022-12-21 08:47:51 +0100

    add tests

--- tests/tests/002_invoices.php
+++ tests/tests/002_invoices.php
@@ -419,6 +419,140 @@ try {
 	test_finished(false);
 }
 
+test_start('invoice - zero amount');
+try {
+    $request = $lexoffice->create_invoice([
+        'voucherDate' => substr(date('c'), 0, 19).'.000'.substr(date('c'), 19),
+        'introduction' => 'Einleitungstext',
+        'remark' => "Fußzeile\r\nMehrzeilig",
+        'address' => [
+            #'contactId' => '<id>',
+            'name' => 'Frau Jane Doe',
+            'street' => 'Str. 1',
+            'zip' => '12345',
+            'city' => 'Stadt',
+            'countryCode' => 'DE',
+        ],
+        'lineItems' => [
+            [
+                'type' => 'custom',
+                'name' => 'Produktname',
+                'description' => 'Beschreibung',
+                'quantity' => 1,
+                'unitName' => 'Stück',
+                'unitPrice' => [
+                    'currency' => 'EUR',
+                    'netAmount' => 11.99,
+                    'taxRatePercentage' => $taxrate_19,
+                ],
+                #'discountPercentage' => 0,
+            ],
+            [
+                'type' => 'custom',
+                'name' => 'Produktname',
+                'description' => 'Beschreibung',
+                'quantity' => 1,
+                'unitName' => 'Stück',
+                'unitPrice' => [
+                    'currency' => 'EUR',
+                    'netAmount' => -11.99,
+                    'taxRatePercentage' => $taxrate_19,
+                ],
+                #'discountPercentage' => 0,
+            ],
+        ],
+        'totalPrice' => [
+            'currency' => 'EUR',
+            #'totalDiscountAbsolute' => 0,
+            #'totalDiscountPercentage' => 0,
+        ],
+        'taxConditions' => [
+            'taxType' => 'net',
+        ],
+        'shippingConditions' => [
+            'shippingDate' => date(DATE_RFC3339_EXTENDED),
+            'shippingType' => 'delivery',
+        ],
+        'paymentConditions' => [
+            'paymentTermLabel' => 'Vorkasse',
+            'paymentTermDuration' => 1,
+        ],
+    ], true);
+
+    if ($request->id) {
+        test('invoice created - id: '.$request->id);
+
+        test_finished(true);
+    } else {
+        test_finished(false);
+    }
+} catch(lexoffice_exception $e) {
+    test($e->getMessage());
+    test(print_r($e->get_error(), true));
+    test_finished(false);
+}
+
+test_start('invoice - 100% discount');
+try {
+    $request = $lexoffice->create_invoice([
+        'voucherDate' => substr(date('c'), 0, 19).'.000'.substr(date('c'), 19),
+        'introduction' => 'Einleitungstext',
+        'remark' => "Fußzeile\r\nMehrzeilig",
+        'address' => [
+            #'contactId' => '<id>',
+            'name' => 'Frau Jane Doe',
+            'street' => 'Str. 1',
+            'zip' => '12345',
+            'city' => 'Stadt',
+            'countryCode' => 'DE',
+        ],
+        'lineItems' => [
+            [
+                'type' => 'custom',
+                'name' => 'Produktname',
+                'description' => 'Beschreibung',
+                'quantity' => 1,
+                'unitName' => 'Stück',
+                'unitPrice' => [
+                    'currency' => 'EUR',
+                    'netAmount' => 11.99,
+                    'taxRatePercentage' => $taxrate_19,
+                ],
+                #'discountPercentage' => 0,
+            ],
+        ],
+        'totalPrice' => [
+            'currency' => 'EUR',
+            #'totalDiscountAbsolute' => 0,
+            'totalDiscountPercentage' => 100,
+        ],
+        'taxConditions' => [
+            'taxType' => 'net',
+        ],
+        'shippingConditions' => [
+            'shippingDate' => date(DATE_RFC3339_EXTENDED),
+            'shippingType' => 'delivery',
+        ],
+        'paymentConditions' => [
+            'paymentTermLabel' => 'Vorkasse',
+            'paymentTermDuration' => 1,
+        ],
+    ], true);
+
+    if ($request->id) {
+        test('invoice created - id: '.$request->id);
+
+        test_finished(true);
+    } else {
+        test_finished(false);
+    }
+} catch(lexoffice_exception $e) {
+    test($e->getMessage());
+    test(print_r($e->get_error(), true));
+    test_finished(false);
+}
+
+
 test_start('invoice - text position');
 try {
 	$request = $lexoffice->create_invoice([
