40c18489d8c3f39c30dffd1f12e42dfffde2d780
Sebastian Hayer-Lutz <slu@baebeca.de>
2023-01-18 21:35:38 +0100

    add tests

--- tests/tests/002_invoices.php
+++ tests/tests/002_invoices.php
@@ -246,7 +246,6 @@ try {
 	test_finished(false);
 }
 
-
 test_start('invoice - 0%, 19, 7% UST position');
 try {
 	$request = $lexoffice->create_invoice([
@@ -736,6 +735,66 @@ try {
 		test(print_r($e->get_error(), true));
 		test_finished(false);
 	}
+}
+
+test_start('invoice - 19% UST position with special chars in product');
+try {
+    $request = $lexoffice->create_invoice([
+        'voucherDate' => substr(date('c'), 0, 19).'.000'.substr(date('c'), 19),
+        'introduction' => 'Einleitungstext',
+        'remark' => "Fußzeile\r\nMehrzeilig",
+        'address' => [
+            #'contactId' => '<id>',
+            'name' => 'Frau Jane Doe',
+            'street' => 'Str. 1',
+            'zip' => '12345',
+            'city' => 'Stadt',
+            'countryCode' => 'DE',
+        ],
+        'lineItems' => [
+            [
+                'type' => 'custom',
+                'name' => 'WEBINAR BLUB ABC - Duminică, 01.01.2000 la ora 13.00',
+                'description' => 'WEBINAR BLUB ABC - Duminică, 01.01.2000 la ora 13.00',
+                'quantity' => 1,
+                'unitName' => 'Stück',
+                'unitPrice' => [
+                    'currency' => 'EUR',
+                    'netAmount' => 11.99,
+                    'taxRatePercentage' => $taxrate_19,
+                ],
+                #'discountPercentage' => 0,
+            ],
+        ],
+        'totalPrice' => [
+            'currency' => 'EUR',
+            #'totalDiscountAbsolute' => 0,
+            #'totalDiscountPercentage' => 0,
+        ],
+        'taxConditions' => [
+            'taxType' => 'net',
+        ],
+        'shippingConditions' => [
+            'shippingDate' => date(DATE_RFC3339_EXTENDED),
+            'shippingType' => 'delivery',
+        ],
+        'paymentConditions' => [
+            'paymentTermLabel' => 'Vorkasse',
+            'paymentTermDuration' => 1,
+        ],
+    ], true);
+
+    if ($request->id) {
+        test('invoice created - id: '.$request->id);
+
+        test_finished(true);
+    } else {
+        test_finished(false);
+    }
+} catch(lexoffice_exception $e) {
+    test($e->getMessage());
+    test(print_r($e->get_error(), true));
+    test_finished(false);
 }
 
 /** only for special tests
