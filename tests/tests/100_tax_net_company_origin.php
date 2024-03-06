1de49383c0708a44967efa75031009ec67fe97c9
Valeriia Bykovets <vby@baebeca.de>
2024-03-06 15:55:31 +0100

    #201925 remove false test

--- tests/tests/100_tax_net_company_origin.php
+++ tests/tests/100_tax_net_company_origin.php
@@ -11,14 +11,4 @@ catch (lexoffice_exception $e) {
     test_finished(false);
 }
 
-test_start('check voucher booking id - romania sell after oss - 19%');
-try {
-    $request = $lexoffice->get_needed_voucher_booking_id(19, 'ro', strtotime('2021-12-27'), false, true, false);
-    test_finished($request === 'd73b880f-c24a-41ea-a862-18d90e1c3d82');
-}
-catch (lexoffice_exception $e) {
-    test($e->getMessage());
-    test_finished(false);
-}
-
 $lexoffice->test_clear_profile();
